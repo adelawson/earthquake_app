1b2dc0256ae61f6b6f5e33f7c68db1c5a3de1e92
adelawson <30274219+adelawson@users.noreply.github.com>
2021-09-17 11:59:32 +0100

    Created recyclerview and loaded app with dummy data

--- app/src/main/java/com/example/earthquakechecker/CityItem.kt
+++ app/src/main/java/com/example/earthquakechecker/CityItem.kt
@@ -2,11 +2,34 @@ package com.example.earthquakechecker
 
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
-class CityItem(var magnitude: Double, var cityName:String, var date:Int) {
+data class  CityItem(var magnitude: Double, var cityName:String, var date:Int) {
 
     fun getDate():String{
+        val long = (date*1000).toLong()
         val sdr = SimpleDateFormat("dd/mm/yy", Locale.ENGLISH)
-        return sdr.format(date)
+        return sdr.format(long)
     }
+}
+object SampleCities{
+    private val magnitudes = arrayListOf(7.1,2.3,4.5,5.6)
+    private val cities = arrayListOf("texas","oklahoma","yemen","naija")
+    private val dates = arrayListOf(1631835870,1631839870,1631839870,1631839870)
+
+
+    var list:ArrayList<CityItem>?= null
+        get() {
+            if (field!=null)
+                return field
+            field = ArrayList()
+            for(i in magnitudes.indices){
+                val magnitude = magnitudes[i]
+                val city = cities[i]
+                val date = dates[i]
+                val cityItem = CityItem(magnitude,city,date)
+                field!!.add(cityItem)
+            }
+            return field
+        }
 }
--- app/src/main/java/com/example/earthquakechecker/EarthquakeAdapter.kt
+++ app/src/main/java/com/example/earthquakechecker/EarthquakeAdapter.kt
@@ -1,4 +1,47 @@
 package com.example.earthquakechecker
 
-class EarthquakeAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class EarthquakeAdapter(val context: Context, var cityList :ArrayList<CityItem> ): RecyclerView.Adapter<EarthquakeAdapter.viewHolder>() {
+
+    inner class viewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private var currentPosition:Int = -1
+        private var currentCity: CityItem? = null
+
+        private val magnitudeTextView = itemView.findViewById<TextView>(R.id.magnitude_txv)
+        private val cityTextView = itemView.findViewById<TextView>(R.id.city_txv)
+        private val dateTextView = itemView.findViewById<TextView>(R.id.date_txv)
+
+        fun setData(city:CityItem,position: Int){
+            magnitudeTextView.text = city.magnitude.toString()
+            cityTextView.text = city.cityName
+            dateTextView.text= city.getDate()
+
+            this.currentPosition = position
+            this.currentCity = city
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
+        val itemView = LayoutInflater.from(context).inflate(R.layout.earthquake_itemview,parent,false)
+        return viewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(viewHolder: viewHolder, position: Int) {
+        val city = cityList[position]
+        viewHolder.setData(city,position)
+    }
+
+    override fun getItemCount(): Int = cityList.size
+
+
 }
